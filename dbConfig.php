3cf8cd5b5d9229b14b1272049a47822d61cb46de
RebPlaras <167282513+RebPlaras@users.noreply.github.com>
2024-10-16 14:31:43 +0800

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -11,7 +11,7 @@ try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Optionally echo this for testing; you can remove it later
+
     echo "Connected successfully"; 
 } catch (PDOException $e) {
     // error message if connection fails
